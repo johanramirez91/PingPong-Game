1ced05034ecc9eb39e915be79282cc0ba452defa
Johan Ramirez <johan911019@gmail.com>
2021-11-19 15:27:22 -0500

    Se define la estructura del canvas en el archivo de javascript

---
+++ PingPong.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('pingpong');
+const WIDTH = 800, HEIGHT = 500;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+canvas.style.background = '#ddd';
+const context = canvas.getContext('2d');
