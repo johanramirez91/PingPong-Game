e3050c84d1afd87e39cd1bc9c81c851068207705
Johan Ramirez <johan911019@gmail.com>
2021-11-19 16:10:54 -0500

    Se escriben las funciones para añadir todas las entidades al canvas, sus estilos y sus posiciones

--- PingPong.js
+++ PingPong.js
@@ -19,28 +19,61 @@ document.addEventListener('keyup', function (event) {
 })
 
 let render = function () {
-    player.render();
+    player.render(0, 0, WIDTH, HEIGHT);
     computer.render();
     ball.render();
 }
 
 let update = function () {
     player.update();
-    computer.update();
-    ball.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
 }
 
 function Player() {
+    this.paddle = new Paddle(WIDTH / 2, HEIGHT - 20, 60, 20);
+}
 
+Player.prototype.render = function () {
+    this.paddle.render();
 }
 
 function Computer() {
+    this.paddle = new Paddle(WIDTH / 2, 0, 60, 20);
+}
 
+Computer.prototype.render = function () {
+    this.paddle.render();
 }
 
 function Ball(x, y) {
     this.x = x;
     this.y = y;
+    this.xSpeed = 0;
+    this.ySpeed = 3;
+    this.radius = 6;
+}
+
+Ball.prototype.render = function () {
+    context.fillStyle = "#111";
+    context.fill();
+}
+
+function Paddle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+}
+
+Paddle.prototype.render = function () {
+
+}
+
+Paddle.prototype.move = function () {
+
 }
 
 function drawGame() {
