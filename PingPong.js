dfd444cf71bce8cd0207721f3a25591a6ef45950
Johan Ramirez <johan911019@gmail.com>
2021-11-19 15:40:09 -0500

    Se añaden las entidades (jugador, computadora, pelota) y las acciones keydown y keyup para manejar el juego con el teclado

--- PingPong.js
+++ PingPong.js
@@ -4,3 +4,18 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 canvas.style.background = '#ddd';
 const context = canvas.getContext('2d');
+
+var keydowns = {};
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(WIDTH / 2 + 10, HEIGHT / 2);
+
+document.addEventListener('keydown', function (event) {
+    keydowns[event.code] = true;
+})
+
+document.addEventListener('keyup', function (event) {
+    delete keydowns[event.code];
+})
+
+
