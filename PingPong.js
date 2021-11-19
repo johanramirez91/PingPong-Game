a79829b943d6da4fe5b0fdfb515d51a94c311e51
Johan Ramirez <johan911019@gmail.com>
2021-11-19 15:52:37 -0500

    Se crean las funciones de cada entidad del juego y la función de dibujar en la pantalla el juego

--- PingPong.js
+++ PingPong.js
@@ -18,4 +18,36 @@ document.addEventListener('keyup', function (event) {
     delete keydowns[event.code];
 })
 
+let render = function () {
+    player.render();
+    computer.render();
+    ball.render();
+}
+
+let update = function () {
+    player.update();
+    computer.update();
+    ball.update();
+}
+
+function Player() {
+
+}
+
+function Computer() {
+
+}
+
+function Ball(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function drawGame() {
+    render();
+    update();
+}
+
+drawGame();
+
 
